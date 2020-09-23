52af82bc438b0948ad5c40c96a4246166df06350
BuildTools <unconfigured@null.spigotmc.org>
2020-09-23 14:31:25 -0600

    Revert "try setup.py for all"
    This reverts commit 8b2615f98d6eb8231be92df13a2ebc275e6956de.
    
    # Conflicts:
    #	layoutlm/__init__.py

--- layoutlm/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-from layoutlm.data.funsd import FunsdDataset
-from .modeling.layoutlm import (
-    LayoutlmConfig,
-    LayoutlmForSequenceClassification,
-    LayoutlmForTokenClassification,
-)
