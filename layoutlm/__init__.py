8b2615f98d6eb8231be92df13a2ebc275e6956de
pa_mike <michael.gibbons@provisionanalytics.io>
2020-09-23 13:46:57 -0600

    try setup.py for all

---
+++ layoutlm/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from layoutlm.layoutlm.seq_labeling import *
+from layoutlm.layoutlm.modeling import *
+from layoutlm.layoutlm.data.funsd import FunsdDataset
+from layoutlm.layoutlm.modeling.layoutlm import (
+    LayoutlmConfig,
+    LayoutlmForSequenceClassification,
+    LayoutlmForTokenClassification,
+)
